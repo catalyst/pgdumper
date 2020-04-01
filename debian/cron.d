e17f771581f21635e8ccfdd40ea915e5d1ae7bb9
Peter Bulmer <peter.bulmer@catalyst.net.nz>
2020-04-01 23:58:53 +1300

    cron - add more example cron lines

--- debian/cron.d
+++ debian/cron.d
@@ -4,3 +4,5 @@
 # If an expert wants to run it twice a day (such as 7am and 10pm), they'll need to change
 # the 'backupinterval' variable in /usr/bin/pgdumper.pl to 43200 (ie seconds in 12 hrs).
 01 22 * * * postgres /usr/bin/pgdumper.pl --dumponprimary --nodumponreplica >> /var/log/pgdumper/pgdumper.log 2>&1
+#01 22 * * * postgres /usr/bin/pgdumper.pl --nodumponprimary --dumponreplica >> /var/log/pgdumper/pgdumper.log 2>&1 #Only dump if this server is a replica.
+#01 22 * * * postgres /usr/bin/pgdumper.pl --dumponprimary --dumponreplica >> /var/log/pgdumper/pgdumper.log 2>&1 #Dump, whether this is a primary, or replica.
